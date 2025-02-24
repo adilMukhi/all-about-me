ce05a2d475ab70b6a8818224bff846fc751efceb
adil <mukhiadil2009@gmail.com>
2025-02-23 22:04:05 -0500

    error fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -6,8 +6,7 @@ export default function Footer() {
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by Your Adil Mukhi. 
-            The source code is available on{" "}
+            Built by Your Adil Mukhi. The source code is available on{" "}
             <a
               href="https://github.com/adilMukhi/all-about-me"
               target="_blank"
