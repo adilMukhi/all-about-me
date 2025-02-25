226c39ea75afdf9afbe76c9036f3fee7909e6a40
adil <mukhiadil2009@gmail.com>
2025-02-24 23:18:17 -0500

    fix 2

--- app/html.tsx
+++ app/html.tsx
@@ -1,21 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8"> <!-- Correct placement -->
-
     <!-- Google Tag Manager -->
-    <script>
-        (function(w,d,s,l,i){
-            w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(), event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0], j=d.createElement(s),
-            dl=l!='dataLayer'?'&l='+l:''; j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-        })(window,document,'script','dataLayer','GTM-KHTXF8N5');
-    </script>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-KHTXF8N5');</script>
     <!-- End Google Tag Manager -->
 
+    <meta charset="UTF-8">
     <!-- Link to main.css -->
     <link href="main.css" rel="stylesheet">
 
@@ -25,17 +19,17 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Kirang+Haerang&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&display=swap">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Times+New+Roman&display=swap">
+    <!-- Title = DynaPuff  -->
+    <!-- SubTitle = Sour Gummy -->
+    <!-- Header = Bubblegum Sans -->
+    <!-- Paragraph = Times New Roman -->
+    <!-- Footer/links = Kirang Haerang -->
 
-    <title>Website Title</title>
 </head>
 <body>
-
     <!-- Google Tag Manager (noscript) -->
-    <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHTXF8N5"
-        height="0" width="0" style="display:none;visibility:hidden"></iframe>
-    </noscript>
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KHTXF8N5"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
-
 </body>
 </html>
