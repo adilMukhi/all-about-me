2efd1a3342ae76a5cc1a2052a2cdce13ea1972ea
adil <mukhiadil2009@gmail.com>
2025-02-24 22:18:01 -0500

    skills

--- components/skills.tsx
+++ components/skills.tsx
@@ -31,7 +31,7 @@ export default function Skills() {
     <section id="skills" className="container py-16" style={{ fontFamily: "'Times New Roman', Times, serif" }}>
       <h2 className="mb-12 text-3xl font-bold tracking-tighter sm:text-4xl md:text-5xl" style={{ fontFamily: "Sour Gummy, latin"}}>Skills</h2>
       <div className="grid gap-6 md:grid-cols-2">
-        <Card className="bg-gradient-to-br from-card to-background shadow-md">
+        <Card className={`card-hover-effect`}>
           <CardHeader>
             <CardTitle style={{ fontFamily: "'Bubblegum Sans', 'Times New Roman', serif" }}>Hard Skills</CardTitle>
             <CardDescription>Technical proficiencies</CardDescription>
@@ -46,7 +46,7 @@ export default function Skills() {
             </div>
           </CardContent>
         </Card>
-        <Card className="bg-gradient-to-br from-card to-background shadow-md">
+        <Card className={`card-hover-effect`}>
           <CardHeader>
             <CardTitle style={{ fontFamily: "'Bubblegum Sans', 'Times New Roman', serif" }}>Soft Skills</CardTitle>
             <CardDescription>Interpersonal and professional skills</CardDescription>
