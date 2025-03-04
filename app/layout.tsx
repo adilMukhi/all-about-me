256d12055a888cba3f65ec0dddba88aef8e9dde9
adil <mukhiadil2009@gmail.com>
2025-03-04 16:17:48 -0500

    speed and pinrest

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,12 +2,13 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import "./globals.css"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "Adil Mukhi - Personal Website",
+  title: "Adil Mukhi - Personal Portfolio",
   description: "A showcase of my skills and projects",
   generator: "v0.dev",
 }
@@ -21,10 +22,12 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link rel="icon" href="/favicon.ico" sizes="any" />
+        <meta name="p:domain_verify" content="10e6074a03407f2fd24aed7425a2acd9" />
       </head>
       <body className={inter.className}>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   )
